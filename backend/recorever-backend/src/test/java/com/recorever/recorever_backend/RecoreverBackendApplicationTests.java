c1b68b53e5a7e7bb7857e06443ff0133062c84e1
Riomalos-ZyrrahFeil <zriomalos@gmail.com>
2025-10-22 00:00:17 +0800

    chore(backend): initial project structure

---
+++ backend/recorever-backend/src/test/java/com/recorever/recorever_backend/RecoreverBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recorever.recorever_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecoreverBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
