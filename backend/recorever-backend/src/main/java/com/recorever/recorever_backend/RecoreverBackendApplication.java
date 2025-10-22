63e463ec52c12474b0e9e8ac7e7f13deeb012d66
riomalos-zyrrahfeil <zriomalos@gmail.com>
2025-10-22 16:48:33 +0800

    chore(backend): initial project structure (#18)

---
+++ backend/recorever-backend/src/main/java/com/recorever/recorever_backend/RecoreverBackendApplication.java
@@ -0,0 +1,13 @@
+package com.recorever.recorever_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecoreverBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecoreverBackendApplication.class, args);
+	}
+
+}
