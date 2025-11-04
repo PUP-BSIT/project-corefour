dae48f2e2713efceabeb388aeb40d0910eba49c9
florido_maydelyn <maydelynflorido07@gmail.com>
2025-11-05 02:55:14 +0800

    feat(frontend): add report item card component

---
+++ frontend/recorever-frontend/src/app/report-item-grid/report-item-card/report-item-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-item-card',
+  imports: [],
+  templateUrl: './report-item-card.html',
+  styleUrl: './report-item-card.scss',
+})
+export class ReportItemCard {
+
+}
