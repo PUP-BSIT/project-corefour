17be2e3071ccd1940629dc2aa7037929cbee4197
florido_maydelyn <maydelynflorido07@gmail.com>
2025-11-05 02:46:24 +0800

    feat(frontend): add report lost page component

---
+++ frontend/recorever-frontend/src/app/report-lost-page/report-lost-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReportLostPage } from './report-lost-page';
+
+describe('ReportLostPage', () => {
+  let component: ReportLostPage;
+  let fixture: ComponentFixture<ReportLostPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReportLostPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ReportLostPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/recorever-frontend/src/app/report-lost-page/report-lost-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-lost-page',
+  imports: [],
+  templateUrl: './report-lost-page.html',
+  styleUrl: './report-lost-page.scss',
+})
+export class ReportLostPage {
+
+}
