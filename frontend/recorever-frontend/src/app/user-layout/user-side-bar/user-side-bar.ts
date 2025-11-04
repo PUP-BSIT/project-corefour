8fbab78724f8829646e90111576e178e24ca392c
florido-maydelyn <maydelynflorido07@gmail.com>
2025-11-05 06:04:40 +0800

    feat(frontend): add user sidebar component (#65)

---
+++ frontend/recorever-frontend/src/app/user-layout/user-side-bar/user-side-bar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-side-bar',
+  imports: [],
+  templateUrl: './user-side-bar.html',
+  styleUrl: './user-side-bar.scss',
+})
+export class UserSideBar {
+
+}
