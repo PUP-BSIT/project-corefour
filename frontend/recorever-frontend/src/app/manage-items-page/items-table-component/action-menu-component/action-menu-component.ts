a81dca3e59851559f2cc81490e71c749aabab658
florido-maydelyn <maydelynflorido07@gmail.com>
2025-11-05 05:57:35 +0800

    feat(frontend): add action menu component (#70)

---
+++ frontend/recorever-frontend/src/app/manage-items-page/items-table-component/action-menu-component/action-menu-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-action-menu-component',
+  imports: [],
+  templateUrl: './action-menu-component.html',
+  styleUrl: './action-menu-component.scss',
+})
+export class ActionMenuComponent {
+
+}
