8049e447b346d162380bd2258eacc5955c9e87b7
florido_maydelyn <maydelynflorido07@gmail.com>
2025-11-04 23:06:37 +0800

    feat(frontend): add report item grid component

---
+++ frontend/recorever-frontend/src/app/report-item-grid/report-item-grid.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-item-grid',
+  imports: [],
+  templateUrl: './report-item-grid.html',
+  styleUrl: './report-item-grid.scss',
+})
+export class ReportItemGrid {
+
+}
