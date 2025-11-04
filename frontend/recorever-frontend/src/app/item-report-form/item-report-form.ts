596613883fde8110f273ffb4595e778048a9c1b7
florido_maydelyn <maydelynflorido07@gmail.com>
2025-11-05 02:39:33 +0800

    feat(frontend): add item report form component

---
+++ frontend/recorever-frontend/src/app/item-report-form/item-report-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-report-form',
+  imports: [],
+  templateUrl: './item-report-form.html',
+  styleUrl: './item-report-form.scss',
+})
+export class ItemReportForm {
+
+}
