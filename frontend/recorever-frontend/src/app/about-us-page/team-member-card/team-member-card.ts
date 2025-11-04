798b24e96f299fdcdde40f12e42a3ecf96660253
florido_maydelyn <maydelynflorido07@gmail.com>
2025-11-04 23:44:55 +0800

    feat(frontend): add team member card component

---
+++ frontend/recorever-frontend/src/app/about-us-page/team-member-card/team-member-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-member-card',
+  imports: [],
+  templateUrl: './team-member-card.html',
+  styleUrl: './team-member-card.scss',
+})
+export class TeamMemberCard {
+
+}
