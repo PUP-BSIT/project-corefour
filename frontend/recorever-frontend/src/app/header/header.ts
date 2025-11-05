faabb3d180b29503a54bb86ae7961d770aa96004
jallaine <jallainesiervo143@gmail.com>
2025-11-06 03:30:15 +0800

    add ts-header component

--- frontend/recorever-frontend/src/app/header/header.ts
+++ frontend/recorever-frontend/src/app/header/header.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
 
+export class Header {
+  @Input() isLoggedIn = false;
+
+  @Output() loginClicked = new EventEmitter<void>();
+  @Output() registerClicked = new EventEmitter<void>();
+
+  onLogin() {
+    this.loginClicked.emit();
+  }
+
+  onRegister() {
+    this.registerClicked.emit();
+  }
 }
