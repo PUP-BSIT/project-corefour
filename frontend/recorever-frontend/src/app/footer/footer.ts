30ec9861eca34ce85e8a214fa91964d30773a785
siervo-jallaine <jallainesiervo143@gmail.com>
2025-11-06 20:24:38 +0800

    feat(ui-blocks): Add Footer component code (#75)
    * Implement Footer component logic
    
    * add html footer component
    
    * apply footer component styles

--- frontend/recorever-frontend/src/app/footer/footer.ts
+++ frontend/recorever-frontend/src/app/footer/footer.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+interface FooterLink {
+  label: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
 export class Footer {
 
+  protected siteLinks: FooterLink[] = [
+    { label: 'Lost', url: '/lost' },
+    { label: 'Report Lost', url: '/report/lost' },
+    { label: 'Found', url: '/found' },
+    { label: 'Report Found', url: '/report/found' },
+  ];
+
+  protected helpLinks: FooterLink[] = [
+    { label: 'Customer Support', url: '/support' },
+    { label: 'Terms & Conditions', url: '/terms' },
+    { label: 'Privacy Policy', url: '/privacy' },
+  ];
+
+  protected mainLinks: FooterLink[] = [
+    { label: 'Github', url: 'https://github.com/PUP-BSIT/project-corefour' },
+    { label: 'About Us', url: '/about' },
+  ];
 }
