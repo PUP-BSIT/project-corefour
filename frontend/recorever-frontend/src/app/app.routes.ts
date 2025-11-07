55ac98fabe81204d5c45bece0070bde0a71708f2
florido-maydelyn <maydelynflorido07@gmail.com>
2025-11-07 18:36:05 +0800

    chore(frontend): add route for public pages of the app (#82)
    * chore(frontend): remove the template for app.html
    
    * chore(frontend): add route for public pages of the app

--- frontend/recorever-frontend/src/app/app.routes.ts
+++ frontend/recorever-frontend/src/app/app.routes.ts
@@ -1,6 +1,29 @@
 import { Routes } from '@angular/router';
+
+import { HeaderNFooterOnly } from './layout/header-nfooter-only/header-nfooter-only';
+import { HeaderOnly } from './layout/header-only/header-only';
+
+import { Homepage } from './page/public/homepage/homepage';
+import { AboutUsPage } from './page/public/about-us-page/about-us-page';
 import { LoginPage } from './page/public/login-page/login-page';
+import { RegisterPage } from './page/public/register-page/register-page';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginPage },
+  {
+    path: '',
+    component: HeaderNFooterOnly,
+    children: [
+      { path: '', component: Homepage },
+      {  path: 'about-us', component: AboutUsPage}
+    ],
+  },
+
+  {
+    path: '',
+    component: HeaderOnly,
+    children: [
+      { path: 'login', component: LoginPage },
+      { path: 'register', component: RegisterPage },
+    ],
+  },
 ];
