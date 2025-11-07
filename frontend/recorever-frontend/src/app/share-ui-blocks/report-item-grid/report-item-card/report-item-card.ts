c19540a51eb278984a64d7bfd812df8bf939daa9
jallaine <jallainesiervo143@gmail.com>
2025-11-08 03:33:28 +0800

    Define as a 'dumb' component, accepts item data via @Input() and emits a click event via @Output() (ticketClicked).

--- frontend/recorever-frontend/src/app/share-ui-blocks/report-item-grid/report-item-card/report-item-card.ts
+++ frontend/recorever-frontend/src/app/share-ui-blocks/report-item-grid/report-item-card/report-item-card.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-report-item-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './report-item-card.html',
   styleUrl: './report-item-card.scss',
 })
 export class ReportItemCard {
 
+  @Input() item: any = {
+    userInitials: 'A',
+    userName: 'User Name',
+    postDate: 'Date',
+    title: 'Title',
+    location: 'Location',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,' +
+        ' sed do eiusmod tempor',
+  };
+
+  @Output() ticketClicked = new EventEmitter<void>();
+
+  onTicketClick(): void {
+    this.ticketClicked.emit();
+    console.log('Ticket button clicked on the card.');
+  }
 }
