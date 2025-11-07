6e2f4ce39e6d62031064182786719d26d3056b21
florido_maydelyn <maydelynflorido07@gmail.com>
2025-11-07 18:24:21 +0800

    feat(frontend): add necessary imports to header-nfooter-only component

--- frontend/recorever-frontend/src/app/layout/header-nfooter-only/header-nfooter-only.ts
+++ frontend/recorever-frontend/src/app/layout/header-nfooter-only/header-nfooter-only.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Footer } from '../../share-ui-blocks/footer/footer';
+import { Header } from '../../share-ui-blocks/header/header';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header-nfooter-only',
-  imports: [],
+  imports: [RouterModule, Footer, Header],
   templateUrl: './header-nfooter-only.html',
   styleUrl: './header-nfooter-only.scss',
 })
