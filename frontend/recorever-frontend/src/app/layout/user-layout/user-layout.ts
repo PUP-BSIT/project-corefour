485c175283131e58e66a4baa2e05025019a4c6d1
jallaine <jallainesiervo143@gmail.com>
2025-11-15 10:23:47 +0800

    fix(frontend-layout): add header and fix sidebar position

--- frontend/recorever-frontend/src/app/layout/user-layout/user-layout.ts
+++ frontend/recorever-frontend/src/app/layout/user-layout/user-layout.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { UserSideBar } from './user-side-bar/user-side-bar';
+import { Header } from '../../share-ui-blocks/header/header';
 
 @Component({
   selector: 'app-user-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, UserSideBar],
+  imports: [CommonModule, RouterOutlet, UserSideBar, Header],
   templateUrl: './user-layout.html',
   styleUrl: './user-layout.scss',
 })
