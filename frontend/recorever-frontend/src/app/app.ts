261384de2b4664e77f285a249bc43815fd4db8c0
jallaine <jallainesiervo143@gmail.com>
2025-11-29 02:33:46 +0800

    feat(frontend): Implement Centered Logo Loading Screen on App Startup

--- frontend/recorever-frontend/src/app/app.ts
+++ frontend/recorever-frontend/src/app/app.ts
@@ -1,6 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit, AfterViewInit, Inject }
+    from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from './core/auth/auth-service';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +10,28 @@ import { AuthService } from './core/auth/auth-service';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+
+export class App implements OnInit, AfterViewInit {
   protected readonly title = signal('recorever-frontend');
-  
-  constructor(private auth: AuthService) {}
+
+  constructor(
+    private auth: AuthService,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
 
   ngOnInit() {
     this.auth.initAuth().subscribe();
   }
 
+  ngAfterViewInit(): void {
+    const loadingScreen = this.document.getElementById('app-loading-screen');
+
+    if (loadingScreen) {
+      loadingScreen.style.opacity = '0';
+
+      setTimeout(() => {
+        loadingScreen.remove();
+      }, 1000);
+    }
+  }
 }
