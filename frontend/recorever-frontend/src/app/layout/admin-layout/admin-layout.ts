0e7531495cd48d6d629f27d75f096d435dd5c795
riomalos-zyrrahfeil <zriomalos@gmail.com>
2025-12-02 23:47:26 +0800

    feat(frontend-admin-ui): Implement and style base Admin View (#150)
    * feat(frontend-admin-ui): Implement base Admin layout structure
    
    The layout includes:
    * Integration of the <app-header> component.
    * Sidebar area hosting the <app-admin-side-bar> component.
    * Main content area using the <router-outlet> for dynamic route rendering.
    
    * feat(frontend-admin-ui): Configure AdminLayout component
    
    Convert the AdminLayout component to a standalone component and configure necessary imports.
    
    This enables the AdminLayout to integrate core components like the Header and AdminSideBar, and use the RouterOutlet to render child routes.
    
    * style(frontend-admin-ui): Style base AdminLayout structure
    
    Add SCSS styling to define the main structural layout of the entire admin viewport.
    * The fixed dimensions for the header and sidebar.

--- frontend/recorever-frontend/src/app/layout/admin-layout/admin-layout.ts
+++ frontend/recorever-frontend/src/app/layout/admin-layout/admin-layout.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router'; 
+import { AdminSideBar} from './admin-side-bar/admin-side-bar'; 
+import { Header } from '../../share-ui-blocks/header/header';
 
 @Component({
-  selector: 'app-admin-layout',
-  imports: [],
-  templateUrl: './admin-layout.html',
-  styleUrl: './admin-layout.scss',
+  selector: 'app-admin-layout',
+  standalone: true, 
+  imports: [RouterOutlet, AdminSideBar, Header], 
+  templateUrl: './admin-layout.html',
+  styleUrls: ['./admin-layout.scss'],
 })
 export class AdminLayout {
 
