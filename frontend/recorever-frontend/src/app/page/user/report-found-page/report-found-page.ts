96285e30f06d48a7a9291e806a46c96019a42a09
jallaine <jallainesiervo143@gmail.com>
2025-12-05 21:01:12 +0800

    Logic updated to receive the `surrender_code` from the backend and populate two signals.

--- frontend/recorever-frontend/src/app/page/user/report-found-page/report-found-page.ts
+++ frontend/recorever-frontend/src/app/page/user/report-found-page/report-found-page.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 import { catchError, of, tap } from 'rxjs';
@@ -6,11 +6,14 @@ import { ItemReportForm }
     from '../../../share-ui-blocks/item-report-form/item-report-form';
 import { FinalReportSubmission, Report } from '../../../models/item-model';
 import { ItemService } from '../../../core/services/item-service';
+import { CodesModal } from '../../../modal/codes-modal/codes-modal';
+
+type ReportWithCodes = Report & { surrender_code: string | null };
 
 @Component({
   selector: 'app-report-found-page',
   standalone: true,
-  imports: [CommonModule, RouterModule, ItemReportForm],
+  imports: [CommonModule, RouterModule, ItemReportForm, CodesModal],
   templateUrl: './report-found-page.html',
   styleUrls: ['./report-found-page.scss']
 })
@@ -18,12 +21,22 @@ export class ReportFoundPage {
 
   private router = inject(Router);
   private itemService = inject(ItemService);
+  protected showReferenceModal = signal(false);
+  protected referenceCode = signal('');
+  protected submissionDate = signal('');
 
   handleSubmission(data: FinalReportSubmission): void {
     this.itemService.createReport(data).pipe(
-      tap((response: Report) => {
+      tap((response: ReportWithCodes) => {
         console.log('Found Report Submitted Successfully:', response);
-        this.router.navigate(['/app/found-items']);
+
+        if (response.surrender_code) {
+          this.referenceCode.set(response.surrender_code);
+          this.submissionDate.set(new Date().toLocaleString());
+          this.showReferenceModal.set(true);
+        } else {
+          this.router.navigate(['/app/found-items']);
+        }
       }),
       catchError(error => {
         console.error('Found Report Submission Failed:', error);
@@ -33,6 +46,11 @@ export class ReportFoundPage {
   }
 
   handleCancel(): void {
+    this.router.navigate(['/app/found-items']);
+  }
+
+  onModalClose(): void {
+    this.showReferenceModal.set(false);
     this.router.navigate(['/app/found-items']);
   }
 }
