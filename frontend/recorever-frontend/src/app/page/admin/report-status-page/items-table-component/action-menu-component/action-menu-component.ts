dde6fc94a35bbd47376d3220a98f0833640d8ccf
Riomalos-ZyrrahFeil <zriomalos@gmail.com>
2025-12-05 23:34:10 +0800

    feat(frontend-action-menu): Finalize ActionMenuComponent functionality

--- frontend/recorever-frontend/src/app/page/admin/report-status-page/items-table-component/action-menu-component/action-menu-component.ts
+++ frontend/recorever-frontend/src/app/page/admin/report-status-page/items-table-component/action-menu-component/action-menu-component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Report } from '../../../../../models/item-model';
 
 @Component({
   selector: 'app-action-menu-component',
   standalone: true,
-  imports: [CommonModule], 
+  imports: [CommonModule],
   templateUrl: './action-menu-component.html',
-  styleUrl: './action-menu-component.scss',
+  styleUrls: ['./action-menu-component.scss'],
 })
 export class ActionMenuComponent {
+  @Input({ required: true }) report!: Report;
 
+  @Output() viewDetails = new EventEmitter<Report>();
+
+  onViewDetails(): void {
+    this.viewDetails.emit(this.report);
+  }
 }
