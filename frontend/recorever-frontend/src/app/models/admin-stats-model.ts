01ddb186dc3021eca466dd504aa8f06366ab8bc6
durante_stephanie <durantestephanie07@gmail.com>
2025-12-19 11:27:27 +0800

    feat(frontend-admin-model): update DashboardData to use ReportStat for reportsOverTime

--- frontend/recorever-frontend/src/app/models/admin-stats-model.ts
+++ frontend/recorever-frontend/src/app/models/admin-stats-model.ts
@@ -1,16 +1,16 @@
+export type ReportStat = {
+  date: string;
+  count: number;
+}
+
 export type DashboardStats = {
   totalReports: number;
   successfullyClaimed: number;
   pendingAction: number;
   lostFoundRatio: string;
-};
-
-export type ChartData = {
-  label: string;
-  value: number;
-};
+}
 
 export type DashboardData = {
   stats: DashboardStats;
-  reportsOverTime: ChartData[];
-};
+  reportsOverTime: ReportStat[];
+}
