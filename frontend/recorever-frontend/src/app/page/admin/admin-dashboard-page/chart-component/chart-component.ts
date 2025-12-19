675224839622e6765c679aa263a7d1f35657e89b
durante_stephanie <durantestephanie07@gmail.com>
2025-12-19 08:48:24 +0800

    feat(frontend-ChartComponent): implement chart component with dynamic data binding and styling

--- frontend/recorever-frontend/src/app/page/admin/admin-dashboard-page/chart-component/chart-component.ts
+++ frontend/recorever-frontend/src/app/page/admin/admin-dashboard-page/chart-component/chart-component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChartData } from '../../../../models/admin-stats-model';
 
 @Component({
   selector: 'app-chart-component',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './chart-component.html',
   styleUrl: './chart-component.scss',
 })
 export class ChartComponent {
-
+  @Input() data: ChartData[] = [];
 }
