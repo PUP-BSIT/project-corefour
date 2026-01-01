adafa70943c93383527093d163757d55b518193a
Riomalos-ZyrrahFeil <zriomalos@gmail.com>
2026-01-01 16:53:21 +0800

    feat(frontend-forgot-password): implement forgot password page

---
+++ frontend/recorever-frontend/src/app/page/public/forgot-pass-page/forgot-pass-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-pass-page',
+  imports: [],
+  templateUrl: './forgot-pass-page.html',
+  styleUrl: './forgot-pass-page.scss',
+})
+export class ForgotPassPage {
+
+}
