bb37238c8ee277f98d230b5393387a38b0632f54
Riomalos-ZyrrahFeil <zriomalos@gmail.com>
2026-01-01 20:48:51 +0800

    feat(frontend-reset-password): implement password reset page

---
+++ frontend/recorever-frontend/src/app/page/public/reset-pass-page/reset-pass-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-pass-page',
+  imports: [],
+  templateUrl: './reset-pass-page.html',
+  styleUrl: './reset-pass-page.scss',
+})
+export class ResetPassPage {
+
+}
