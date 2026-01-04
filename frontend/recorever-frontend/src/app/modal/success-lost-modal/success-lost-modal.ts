4e8a8f9d5426ff341cda0de07f27341e5120f63e
Riomalos-ZyrrahFeil <zriomalos@gmail.com>
2026-01-05 00:47:57 +0800

    feat(frontend-lost-status): create success-lost-modal component

---
+++ frontend/recorever-frontend/src/app/modal/success-lost-modal/success-lost-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-lost-modal',
+  imports: [],
+  templateUrl: './success-lost-modal.html',
+  styleUrl: './success-lost-modal.scss',
+})
+export class SuccessLostModal {
+
+}
